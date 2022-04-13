0bc48083be8ea8d86d86e524378c9388fb09b86a
Aurélie Neveu <aureliechabannes@gmail.com>
2022-04-13 18:08:13 +0200

    Calculatrice avec gestion des erreurs

--- calculatrice.py
+++ calculatrice.py
@@ -1,12 +1,10 @@
-nombre1 = input("Entrez un premier nombre : ")
-if (nombre1.isdigit() == False):
-    print("Veuillez indiquer un chiffre valide.")
-    nombre1 = input("Entrez un premier nombre : ")
+a = b = ""
 
-nombre2 = input("Entrez un deuxième nombre : ")
+while not (a.isdigit() and b.isdigit()):
+    a = input("Entrez un premier nombre : ")
+    b = input("Entrez un deuxième nombre : ")
 
-if (nombre2.isdigit() == False):
-    print("Veuillez indiquer un chiffre valide.")
-    nombre2 = input("Entrez un deuxième nombre : ")
+    if not (a.isdigit() and b.isdigit()):
+        print("Veuillez entrer des chiffres valides.")
 
-print(f"Le résultat de l'addition du nombre {nombre1} avec le nombre {nombre2} est égal à {int(nombre1) + int(nombre2)}")
+print(f"Le résultat de l'addition du nombre {a} avec le nombre {b} est égal à {int(a) + int(b)}")
