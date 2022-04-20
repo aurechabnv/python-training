860999490d6173098abc69ab63e97ba4ca3b4418
Aurélie Neveu <aureliechabannes@gmail.com>
2022-04-20 16:01:36 +0200

    Liste de courses avec sauvegarde sur le disque

--- liste-de-courses/main.py
+++ liste-de-courses/main.py
@@ -1,73 +1,71 @@
-# v1
-# Version simple de la liste de courses
-# Création d'une liste en mémoire avec ajout/retrait d'éléments
-# Pas de sauvegarde sur le disque
+# v2
+# Gestion de liste de courses
+# avec sauvegarde sur le disque dur
 
+import json
+import os
 import sys
 
-liste_de_courses = []
-instructions = """------------------------------------------
-Choisissez parmi les 5 options suivantes :
-1: Ajouter un élément à la liste
-2: Retirer un élément à la liste
-3: Afficher la liste
-4: Vider la liste
-5: Quitter"""
+CUR_DIR = os.path.dirname(__file__)
+CHEMIN_LISTE = os.path.join(CUR_DIR, "liste.json")
+AJOUTER, RETIRER, AFFICHER, VIDER, QUITTER = 1, 2, 3, 4, 5
 
-print("""------------------------------------------
------------ Ma liste de courses ----------""")
+INSTRUCTIONS = f"""Choisissez parmi les 5 options suivantes :
+{AJOUTER}: Ajouter un élément à la liste
+{RETIRER}: Retirer un élément à la liste
+{AFFICHER}: Afficher la liste
+{VIDER}: Vider la liste
+{QUITTER}: Quitter
+\u261b Votre choix : """
+
+if os.path.exists(CHEMIN_LISTE):
+    with open(CHEMIN_LISTE, "r") as f:
+        liste_de_courses = json.load(f)
+else:
+    liste_de_courses = []
+
+print("~o~" * 5 + " Ma liste de courses " + "~o~" * 5)
 
 while True:
-    print(instructions)
-    action = input("\u261b Votre choix : ")
+    action = input(INSTRUCTIONS)
 
     if not action.isdigit():
         print("Veuillez entrer un chiffre valide !")
-        continue
 
-    # Si 5 - QUITTER
-    if int(action) == 5:
-        sys.exit()
-
-    # Si 1 - AJOUTER
-    elif int(action) == 1:
+    elif int(action) == AJOUTER:
         element = input("Entrez le nom de l'élément à ajouter : ")
         liste_de_courses.append(element)
         print(f"L'élément {element} a bien été ajouté à la liste de courses.")
-        continue
 
-    # Si 2 - RETIRER
-    elif int(action) == 2:
+    elif int(action) == RETIRER:
         element = input("Entrez le nom de l'élément à retirer : ")
         if element in liste_de_courses:
             liste_de_courses.remove(element)
             print(f"L'élément {element} a bien été retiré de la liste de courses.")
-            continue
         else:
             print(f"L'élément {element} ne fait pas partie de la liste de courses.")
-            continue
 
-    # Si 3 - AFFICHER
-    elif int(action) == 3:
+    elif int(action) == AFFICHER:
         if len(liste_de_courses) > 0:
             for i, element in enumerate(liste_de_courses):
                 print(f"{i+1}. {element}")
-            continue
         else:
             print("La liste de courses ne contient aucun élément.")
-            continue
 
-    # Si 4 - VIDER
-    elif int(action) == 4:
+    elif int(action) == VIDER:
         if len(liste_de_courses) > 0:
             liste_de_courses.clear()
             print("La liste de courses a bien été vidée.")
-            continue
         else:
             print("La liste de courses est déjà vide.")
-            continue
+
+    elif int(action) == QUITTER:
+        with open(CHEMIN_LISTE, "w") as f:
+            json.dump(liste_de_courses, f)
+        print("A bientôt !")
+        sys.exit()
     
-    # Si un autre chiffre
     else:
         print("Cette option n'est pas disponible.")
-        continue
+    
+    print("-" * 40)
