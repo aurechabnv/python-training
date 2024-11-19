8b0b3ada495276a6c966d368cfbbdbfcd040d0ae
aurechabnv <git@keltpoint.com>
2024-11-19 16:41:14 +0100

    Correction to-do list

--- docstring/framework-pyside/todolist.py
+++ docstring/framework-pyside/todolist.py
@@ -6,30 +6,27 @@ class MainWindow(QWidget):
 
         self.setWindowTitle("Ma to-do list")
         
-        main_layout = QVBoxLayout(self)
+        self.main_layout = QVBoxLayout(self)
 
-        self.lw_list = QListWidget()
-        self.le_text = QLineEdit()
-        self.le_text.setPlaceholderText("Tâche à effectuer")
+        self.lw_todos = QListWidget()
+        self.le_task_title = QLineEdit()
+        self.le_task_title.setPlaceholderText("Tâche à effectuer...")
         self.btn_clear = QPushButton("Tout supprimer")
 
-        main_layout.addWidget(self.lw_list)
-        main_layout.addWidget(self.le_text)
-        main_layout.addWidget(self.btn_clear)
+        self.main_layout.addWidget(self.lw_todos)
+        self.main_layout.addWidget(self.le_task_title)
+        self.main_layout.addWidget(self.btn_clear)
 
-        self.lw_list.itemDoubleClicked.connect(self.delete_item)
-        self.le_text.returnPressed.connect(self.add_item)
-        self.btn_clear.clicked.connect(self.clear_list)
+        self.lw_todos.itemDoubleClicked.connect(self.delete_item)
+        self.le_task_title.returnPressed.connect(self.add_item)
+        self.btn_clear.clicked.connect(self.lw_todos.clear)
 
     def delete_item(self, item):
-        self.lw_list.takeItem(self.lw_list.row(item))
+        self.lw_todos.takeItem(self.lw_todos.row(item))
 
     def add_item(self):
-        self.lw_list.addItem(QListWidgetItem(self.le_text.text()))
-        self.le_text.clear()
-
-    def clear_list(self):
-        self.lw_list.clear()
+        self.lw_todos.addItem(self.le_task_title.text())
+        self.le_task_title.clear()
         
 
 app = QApplication()
