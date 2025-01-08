a33327f63832494039005463be78b1a1891a7a39
aurechabnv <git@keltpoint.com>
2025-01-08 16:41:59 +0100

    Add BlogPost model

--- framework-django/src/blog/models.py
+++ framework-django/src/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    published = models.BooleanField(default=False)
+    date = models.DateField(blank=True)
+    content = models.TextField()
