021281e24448061ebe7e1664a22fd0f473f311fc
aurechabnv <git@keltpoint.com>
2025-01-08 17:41:19 +0100

    Set slug programmatically

--- framework-django/src/blog/models.py
+++ framework-django/src/blog/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(blank=True)
     published = models.BooleanField(default=False)
     date = models.DateField(blank=True, null=True)
     content = models.TextField()
     description = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
