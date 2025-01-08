fa310841aab0db1733b7c7cd7b4263a8d1b126c8
aurechabnv <git@keltpoint.com>
2025-01-08 15:03:01 +0100

    Rename website folder

--- framework-django/src/website/views.py
+++ framework-django/src/website/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def index(request):
     date = datetime.today()
-    return render(request, 'DjangoExample/index.html', context={'today':date})
+    return render(request, 'website/index.html', context={'today':date})
