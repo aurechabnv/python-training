6d96c7097136729e0052a8bde0238669cdd01833
aurechabnv <git@keltpoint.com>
2025-01-09 17:40:48 +0100

    Add relations 1-n with author and n-n with categories

--- framework-django/src/blog/models.py
+++ framework-django/src/blog/models.py
@@ -1,8 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.text import slugify
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=36)
+    slug = models.SlugField()
+
+
 class BlogPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=100)
     slug = models.SlugField(blank=True)
     published = models.BooleanField(default=False)
