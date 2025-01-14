9d26d708edab24bc817fb82f294f9e638364afbd
aurechabnv <git@keltpoint.com>
2025-01-14 12:32:21 +0100

    Create a view for blog posts and manage 404 error

--- framework-django/src/blog/urls.py
+++ framework-django/src/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index, article
+from .views import index, article, blog_post
 
 urlpatterns = [
     path('', index, name='blog-index'),
     path('article_<str:numero_article>/', article, name='blog-article'),
+    path('<str:slug>/', blog_post, name='blog-post'),
 ]
--- framework-django/src/blog/views.py
+++ framework-django/src/blog/views.py
@@ -1,4 +1,9 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
+
+from blog.models import BlogPost
+
 
 def index(request):
     return render(request, 'blog/index.html')
@@ -7,3 +12,13 @@ def article(request, numero_article):
     if numero_article in ['01', '02', '03']:
         return render(request, f'blog/article_{numero_article}.html')
     return render(request, 'blog/article_not_found.html')
+
+def blog_post(request, slug):
+    try:
+        post = BlogPost.objects.get(slug=slug)
+    except BlogPost.DoesNotExist:
+        raise Http404(f"L'article '{slug}' n'existe pas")
+
+    # response = HttpResponse(render_to_string('blog/post.html', context={'blog_post': post}))
+    response = render(request, 'blog/post.html', context={'blog_post': post})
+    return response
